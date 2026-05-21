fa001c67fca4a7afa86937fbdd82b0476f7c200e
Shashuvo <shahariat2810@gmail.com>
2026-05-21 20:57:19 +0600

    Send response function added for thin model

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { urlencoded, type Request, type Response } from "express";
+import sendResponse from "./utility/sendResponse";
 const app = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(urlencoded({ extended: true }));
 
-app.get("/", async(req:Request, res: Response)=>{
-    res.status(200).json({
+app.get("/", async (req: Request, res: Response) => {
+    sendResponse(res, {
+        statusCode: 200,
         success: true,
         message: "This is root route of Dev Pulse"
     })
